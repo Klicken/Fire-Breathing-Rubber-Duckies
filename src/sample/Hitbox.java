0c5501b1ec71111fdf857f612030a908f4ce297f
Tobias Rosengren <Tobias@Tobiass-MacBook-Pro.local>
2018-01-24 11:23:47 +0100

    Added comments for the Hitbox class

--- src/sample/Hitbox.java
+++ src/sample/Hitbox.java
@@ -4,12 +4,27 @@ import javafx.geometry.Point2D;
 import static java.lang.Math.abs;
 import javafx.scene.image.Image;
 
+/*
+*   A class for creating hitboxes which can be used for collison detection
+*   between different game objects.
+*/
+
+/*
+*   position: The coordinate of the top left corner of the hitbox (x, y)
+*   width: Sets the width of the hitbox (left to right)
+*   height: Sets the height of the hitbox (top to bottom)
+*/
+
 public class Hitbox
 {
     private Point2D position;
     int width;
     int height;
 
+    /*
+    *   Constructor that takes an x- and y-coordinate to set the top left corner position
+    *   of the hitbox. width and height can be set manually.
+    */
     public Hitbox(int x, int y, int width, int height)
     {
         position = new Point2D(x, y);
@@ -17,12 +32,22 @@ public class Hitbox
         this.height = height;
     }
 
+    /*
+     *   Constructor that takes an x- and y-coordinate to set the top left corner position
+     *   of the hitbox. width and height are set according to loaded image-file.
+     */
+
     public Hitbox(int x, int y, Image img)
     {
         position = new Point2D(x, y);
         this.width = (int)img.getWidth();
         this.height = (int)img.getHeight();
     }
+
+    /*
+    *   Checks if this hitbox intersects another hitbox.
+    *   If it does, the method returns true, else false.
+    */
 
     public boolean intersects(Hitbox other)
     {
