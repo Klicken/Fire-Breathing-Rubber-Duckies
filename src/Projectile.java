ff5d82ed1ab51b9f391c25e3008f65776f7fccb4
Anton <anton@Antons-MacBook-Pro.local>
2018-01-26 13:27:10 +0100

    Updated variable angle

--- src/Projectile.java
+++ src/Projectile.java
@@ -4,18 +4,18 @@ import javafx.scene.image.Image;
 
 public class Projectile extends DynamicGameObject
 {
-    private Point2D angle;
+    private double angle;
     private int damage;
 
 
-    public Projectile(Point2D position, Image image, Point2D direction, double speed, Point2D angle, int damage)
+    public Projectile(Point2D position, Image image, Point2D direction, double speed, double angle, int damage)
     {
         super(position, image, direction, speed);
         this.angle = angle;
         this.damage = damage;
     }
 
-    public Projectile(Point2D position, Hitbox hitbox, Image image, Point2D angle, int damage,double speed)
+    public Projectile(Point2D position, Hitbox hitbox, Image image, double angle, int damage,double speed)
     {
         super(position, hitbox, image, speed);
         this.angle = angle;
