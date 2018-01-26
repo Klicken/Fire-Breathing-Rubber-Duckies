026fe19e2698bd1c702f590412dd5dd1ecc40fa8
Anton <anton@Antons-MacBook-Pro.local>
2018-01-26 14:36:24 +0100

    Added comments for Projectile constructors

--- src/Projectile.java
+++ src/Projectile.java
@@ -7,12 +7,25 @@ public class Projectile extends DynamicGameObject
     private double angle;
     private int damage;
 
+    /*
+     *   Constructor that creates a Projectile object with a given damage value.
+     *   Position, dimensions and hitbox are set according to
+     *   the dimensions of the image-file.
+     *   speed is set independantly.
+     */
+
     public Projectile(Point2D position, Image image, double speed, int damage)
     {
         super(position, image, speed);
         this.damage = damage;
     }
 
+    /*
+     *   Constructor that creates a Projectile object with a given damage value.
+     *  Position and dimensions are set according to the dimensions
+     *   of the image, speed and hitbox are set independantly. If you for instance want a hitbox of different size then
+     *   the Projectile image.
+     */
     public Projectile(Point2D position, Hitbox hitbox, Image image, double speed, int damage)
     {
         super(position, hitbox, image, speed);
