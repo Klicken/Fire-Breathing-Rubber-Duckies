a1741b394b0c5a4e67bdf849b76dd28b87fa2dc4
oscarnnn <35729517+oscarnnn@users.noreply.github.com>
2018-01-26 11:47:11 +0100

    Added highscore.java

---
+++ src/Highscore.java
@@ -0,0 +1,28 @@
+public class Highscore {
+
+
+    private int score;
+    private String name;
+
+    public Highscore(int score, String name)
+    {
+        this.score = score;
+        this.name = name;
+    }
+
+
+    public void sortHighscore(Highscore[] obj)
+    {
+
+    }
+
+    public void writeToFile()
+    {
+
+    }
+
+    public void readFromFile()
+    {
+
+    }
+}
